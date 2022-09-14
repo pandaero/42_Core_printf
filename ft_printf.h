202fee5b187ce533ee90f788d983bf8533bb3356
pandalaf <pandalaf@student.42.wolfsburg.de>
2022-09-14 14:37:25 -0300

    began header file

---
+++ ft_printf.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pandalaf <pandalaf@student.42wolfsburg.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/14 14:30:39 by pandalaf          #+#    #+#             */
+/*   Updated: 2022/09/14 14:37:01 by pandalaf         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+//Function prints out a formatted string based on inputs. Returns written chars.
+int		ft_printf(const char *format, ...);
+
+#endif
