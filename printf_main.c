9b98e93a2321a5f01b19580c9cadad6faf62bfba
pandalaf <pandalaf@student.42.wolfsburg.de>
2022-09-14 14:14:17 -0300

    added bonus checks

--- printf_main.c
+++ printf_main.c
@@ -8,7 +8,7 @@ int	main(void)
 	char			*str;
 	unsigned int	u;
 
-	i = -10;
+	i = 10;
 	ch = 'A';
 	str = "Hello";
 	u = 10;
@@ -16,12 +16,15 @@ int	main(void)
 	ft_printf("-----------ft_printf-----------\n");
 	ft_printf("Char: %c, Pointer: %p, String: %s\n", ch, str, str);
 	ft_printf("Sgn int: %i, Sgn dec: %d, Usgn dec: %u\n", i, i, u);
-	ft_printf("Usgn hex l: %x, Usgn hex U: %X\n", u, u);
+	ft_printf("Usgn hex l: %x, Usgn hex U: %X, Percent: %%\n", u, u);
 	//*/
 	printf("--------------printf-----------\n");
 	printf("Char: %c, Pointer: %p, String: %s\n", ch, str, str);
 	printf("Sgn int: %i, Sgn dec: %d, Usgn dec: %u\n", i, i, u);
-	printf("Usgn hex l: %x, Usgn hex U: %X\n", u, u);
+	printf("Usgn hex l: %x, Usgn hex U: %X, Percent: %%\n", u, u);
+	printf("Bonus\n");
+	printf("--flag: %-5d, 0-flag: %05d, .-flag: %.5d\n", i, i, i);
+	printf("#-flag: %#5X, _-flag: % 5d, +-flag: %+d\n", u, i, i);
 	return (0);
 }
 //*/
