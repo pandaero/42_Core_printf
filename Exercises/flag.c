4f105445985e8b326938cc8e0d54b27d31bfceab
pandalaf <pandalaf@student.42.wolfsburg.de>
2022-09-19 20:40:25 -0300

    added exercise to deal with flags

---
+++ Exercises/flag.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int	main(void)
+{
+	char	str[] = "Flagging now: %1234c";
+	char	*out;
+	int		i, j;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] == '%')
+		{
+			j = 0;
+			while (str[i + j] != 'c')
+				j++;
+		}
+		i++;
+	}
+	out = malloc((j + 2) * sizeof(*out));
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] == '%')
+		{
+			j = 0;
+			while (str[i + j + 1] != 'c')
+			{
+				out[j] = str[i + j + 1];
+				j++;
+			}
+		}
+		i++;
+	}
+	printf("In: %s, Flag: %s\n", str, out);
+	free(out);
+	return (0);
+}
