aa5666ec7cd9a5dc5d8270e8e615006afd85e8e2
pandalaf <pandalaf@student.42.wolfsburg.de>
2022-09-25 14:54:59 +0200

    simplified functions slightly

--- flag_interpreter.c
+++ flag_interpreter.c
@@ -6,7 +6,7 @@
 /*   By: pandalaf <pandalaf@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/20 17:00:50 by pandalaf          #+#    #+#             */
-/*   Updated: 2022/09/20 17:35:42 by pandalaf         ###   ########.fr       */
+/*   Updated: 2022/09/20 18:42:54 by pandalaf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,13 +21,6 @@ int	flagtypes(char ch)
 		return (1);
 	else
 		return (0);
-}
-
-//Function returns the types present in the format flag specifier.
-char flagtype(const char *flags)
-{
-	if (flagtypes(flags[0]) == 1)
-		return (flags[0]);
 }
 
 //Function works out the minimum width parameter from a format flag specifier.
