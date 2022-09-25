fe471c795d3ba9e2fb019f455ccc444fb251d7b4
pandalaf <pandalaf@student.42.wolfsburg.de>
2022-09-25 19:30:37 +0200

    edited test for just mandatory part

--- printf_main.c
+++ printf_main.c
@@ -17,19 +17,18 @@ int	main(void)
 	count = 0;
 	count = ft_printf("-----------ft_printf-----------\n");
 	ft_printf("Count: %d\n", count);
-	count = ft_printf("Char: %c\n", ch);//, Pointer: %p, String: %s\n", ch, str, str);
-	/*
 	count = ft_printf("Char: %c, Pointer: %p, String: %s\n", ch, str, str);
-	ft_printf("Line: %d\n", len);
+	ft_printf("Line: %d\n", count);
 	count = ft_printf("Sgn int: %i, Sgn dec: %d, Usgn dec: %u\n", i, i, u);
-	ft_printf("Line: %d\n", len);
+	ft_printf("Line: %d\n", count);
 	count = ft_printf("Usgn hex l: %x, Usgn hex U: %X, Percent: %%\n", u, u);
-	ft_printf("Line: %d\n", len);
+	ft_printf("Line: %d\n", count);
+	/*
 	ft_printf("Bonus\n");
 	count = ft_printf("--flag: %-5d, 0-flag: %05d, .-flag: %.5d\n", i, i, i);
-	ft_printf("Line: %d\n", len);
+	ft_printf("Line: %d\n", count);
 	count = ft_printf("#-flag: %#5X, _-flag: % 5d, +-flag: %+d\n", u, i, i);
-	ft_printf("Line: %d\n", len);count
+	ft_printf("Line: %d\n", count);
 	//*/
 	len = printf("--------------printf-----------\n");
 	printf("Count: %d\n", len);
@@ -39,11 +38,13 @@ int	main(void)
 	printf("Line: %d\n", len);
 	len = printf("Usgn hex l: %x, Usgn hex U: %X, Percent: %%\n", u, u);
 	printf("Line: %d\n", len);
+	/*
 	printf("Bonus\n");
 	len = printf("--flag: %-5d, 0-flag: %05d, .-flag: %.5d\n", i, i, i);
 	printf("Line: %d\n", len);
 	len = printf("#-flag: %#5X, _-flag: % 5d, +-flag: %+d\n", u, i, i);
 	printf("Line: %d\n", len);
+	//*/
 	return (0);
 }
 //*/
