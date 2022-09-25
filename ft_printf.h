cde3fb7e6b58e563f58fd28d875c553f96a7c88c
pandalaf <pandalaf@student.42.wolfsburg.de>
2022-09-25 14:58:11 +0200

    added many flag functions and standard printer one

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: pandalaf <pandalaf@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/14 14:30:39 by pandalaf          #+#    #+#             */
-/*   Updated: 2022/09/20 17:40:00 by pandalaf         ###   ########.fr       */
+/*   Updated: 2022/09/25 09:43:11 by pandalaf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@
 
 //Function prints out a formatted string based on inputs. Returns written chars.
 int		ft_printf(const char *format, ...);
+//Function prints out an integer to the standard output.
+int		ft_print_int(int num);
 //Function prints out a formatted integer based on format flags.
 int		ft_print_int_fmt(int num, const char *flags);
 //Function prints out a formatted unsigned integer based on format flags.
@@ -33,8 +35,20 @@ int		ft_print_ptr_fmt(void *ptr, const char *flags);
 int		types(char ch);
 //Function identifies a character as a format flag type. Returns 1 or 0.
 int		flagtypes(char ch);
+//Function returns whether left align flag is called.
+int		leftalign(const char *flags);
+//Function returns whether zero padding flag is called.
+int		zeropad(const char *flags);
+//Function returns whether precision flag is called.
+int		precision(const char *flags);
+//Function determines whether hex prefix flag is called.
+int		hexpre(const char *flags);
+//Function determines whether blank padding flag is called.
+int		blankpad(const char *flags);
+//Function determines whether plus sign flag is called.
+int		plus(const char *flags);
 //Function works out the minimum width parameter from a format flag specifier.
-int flagwidth(const char *flags);
+int 	flagwidth(const char *flags);
 //Function reads a format variable type after a determined starting location.
 char	typeread(const char *string, int start);
 //Function reads format flags after a determined starting location. Alloc's str.
