6d7bf3c8f78f68b257f5ce75f09b410d13e35fa6
pandalaf <pandalaf@student.42.wolfsburg.de>
2022-09-26 11:36:57 +0200

    Made formatted string printer and rearranged functions

--- padding.c
+++ padding.c
@@ -1,24 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   flag_types_add.c                                   :+:      :+:    :+:   */
+/*   padding.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pandalaf <pandalaf@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/09/20 18:46:19 by pandalaf          #+#    #+#             */
-/*   Updated: 2022/09/20 18:53:36 by pandalaf         ###   ########.fr       */
+/*   Created: 2022/09/26 06:33:55 by pandalaf          #+#    #+#             */
+/*   Updated: 2022/09/26 06:36:13 by pandalaf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//Function determines whether hex prefix flag is called.
-int	hexpre(const char *flags)
+//Function returns whether zero padding flag is called.
+int	zeropad(const char *flags)
 {
 	int	i;
 
 	i = 0;
 	while (flags[i] != '\0')
 	{
-		if (flags[i] == '#')
+		if (flags[i] == '0')
 			return (1);
 		i++;
 	}
@@ -34,21 +34,6 @@ int	blankpad(const char *flags)
 	while (flags[i] != '\0')
 	{
 		if (flags[i] == ' ')
-			return (1);
-		i++;
-	}
-	return (0);
-}
-
-//Function determines whether plus sign flag is called.
-int	plus(const char *flags)
-{
-	int	i;
-
-	i = 0;
-	while (flags[i] != '\0')
-	{
-		if (flags[i] == '+')
 			return (1);
 		i++;
 	}
